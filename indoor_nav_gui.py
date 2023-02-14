7284501f5675145155d7437dd18d2742fd20f8d5
lunaa3 <lunaa3@wwu.edu>
2023-02-13 20:48:30 -0800

    The simple empty gui follows the figma prototype design

--- indoor_nav_gui.py
+++ indoor_nav_gui.py
@@ -3,21 +3,43 @@ import camera_input as camera
 import numpy as np
 import cv2
 from PyQt5 import QtWidgets as qtw
-from PyQt5.QtCore import QUrl, QThread, pyqtSignal, QObject, pyqtSlot
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtCore import QCoreApplication, QMetaObject, QRect, QUrl, QThread, pyqtSignal, QObject, pyqtSlot
 from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
+class App(qtw.QWidget):
+    def setupUI(self, window):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setStyleSheet('background-color: black;')
+        MainWindow.resize(927, 652)
 
-class MainWindow(qtw.QWidget):
-    # Sets up the media player to play warning sound
+        self.CameraView = CameraView(MainWindow)
+
+        self.CollisionIndicatorView = CollisionIndicatorView(MainWindow)
+        
+        self.LogView = LogView(MainWindow)
+        
+        self.SettingsView = SettingsView(MainWindow)
+
+        self.retranslateUi(MainWindow)
+        QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Collision Detection"))
+
+class CameraView(qtw.QWidget):
     media_player = QMediaPlayer()
     file_path = os.path.join(os.getcwd(), "soundfx/warning.mp3")
     url = QUrl.fromLocalFile(file_path)
     content = QMediaContent(url)
     media_player.setMedia(content)
 
-    def __init__(self, *arg, **kwargs):
-        super().__init__(*arg, **kwargs)
+    def __init__(self, ParentWidget):
+        self.CameraView = qtw.QWidget(ParentWidget)
+        self.CameraView.setGeometry(QRect(30, 20, 871, 361))
+        self.CameraView.setObjectName("CameraView")
+        self.CameraView.setStyleSheet('background-color: white;')
 
         self.warning_button = qtw.QPushButton("Warning Button")
         self.warning_button.clicked.connect(self.warning)
@@ -28,9 +50,7 @@ class MainWindow(qtw.QWidget):
         layout = qtw.QVBoxLayout()
         layout.addWidget(self.warning_button)
         layout.addWidget(self.video_label)
-        self.setLayout(layout)
-
-        self.show()
+        self.CameraView.setLayout(layout)
 
     @pyqtSlot(np.ndarray)
     def update_frame(self, frame):
@@ -42,15 +62,36 @@ class MainWindow(qtw.QWidget):
     def warning(self):
         self.media_player.play()
 
+class CollisionIndicatorView(qtw.QWidget):
+    def __init__(self, ParentWidget):
+        self.CollisionIndicatorView = qtw.QWidget(ParentWidget)
+        self.CollisionIndicatorView.setGeometry(QRect(29, 399, 211, 231))
+        self.CollisionIndicatorView.setObjectName("CollisionIndicatorView")
+        self.CollisionIndicatorView.setStyleSheet('background-color: white;')
 
+class LogView(qtw.QWidget):
+    def __init__(self, ParentWidget):
+        self.LogView = qtw.QWidget(ParentWidget)
+        self.LogView.setGeometry(QRect(259, 399, 411, 231))
+        self.LogView.setObjectName("LogView")
+        self.LogView.setStyleSheet('background-color: white;')
+
+class SettingsView(qtw.QWidget):
+    def __init__(self, ParentWidget):
+        self.SettingsView = qtw.QWidget(ParentWidget)
+        self.SettingsView.setGeometry(QRect(690, 400, 211, 231))
+        self.SettingsView.setObjectName("SettingsView")
+        self.SettingsView.setStyleSheet('background-color: white;')
 
 if __name__ == "__main__":
-
     app = qtw.QApplication(sys.argv)
 
     camera_input = camera.CameraInput()
     frame_updater_thread = camera.FrameUpdaterThread(camera_input)
     frame_updater_thread.start()
 
-    w = MainWindow()
-    sys.exit(app.exec())
+    MainWindow = qtw.QWidget()
+    ui = App()
+    ui.setupUI(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
