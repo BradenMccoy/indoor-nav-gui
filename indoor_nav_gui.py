54ad45f1e669c78720b1dd8f5559d6461c39db73
lunaa3 <lunaa3@wwu.edu>
2023-02-16 01:58:45 -0800

    Implemented logging and sound toggle button

--- indoor_nav_gui.py
+++ indoor_nav_gui.py
@@ -4,31 +4,30 @@ import math
 import depthai as dai
 import numpy as np
 import cv2
+import logging
 from PyQt5 import QtWidgets as qtw
 from PyQt5.QtCore import Qt, QCoreApplication, QMetaObject, QRect, QUrl, QThread, pyqtSignal, QObject, pyqtSlot
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap, QImage, QIcon, QFont
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
 class App(qtw.QWidget):
-    def setupUI(self, window):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.setStyleSheet('background-color: black;')
-        MainWindow.resize(927, 652)
+    def setupUI(self, main_window):
+        main_window.setObjectName("MainWindow")
+        main_window.setStyleSheet("background-color: black;")
+        main_window.resize(927, 652)
 
-        self.CameraView = CameraView(MainWindow)
-
-        self.CollisionIndicatorView = CollisionIndicatorView(MainWindow)
+        self.settings_view = SettingsView(main_window)
+        self.camera_view = CameraView(main_window, self.settings_view)
+        self.collision_indicator_view = CollisionIndicatorView(main_window)
+        self.log_view = LogView(main_window)
         
-        self.LogView = LogView(MainWindow)
-        
-        self.SettingsView = SettingsView(MainWindow)
 
-        self.retranslateUi(MainWindow)
-        QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(main_window)
+        QMetaObject.connectSlotsByName(main_window)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, main_window):
         _translate = QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Collision Detection"))
+        main_window.setWindowTitle(_translate("MainWindow", "Collision Detection"))
 
 class CameraView(qtw.QWidget):
     media_player = QMediaPlayer()
@@ -36,22 +35,25 @@ class CameraView(qtw.QWidget):
     url = QUrl.fromLocalFile(file_path)
     content = QMediaContent(url)
     media_player.setMedia(content)
-    show_warning = False
+    show_warning = True
+    auditory_warning = True
 
-    def __init__(self, ParentWidget):
+    def __init__(self, parent_widget, settings):
+        super(CameraView, self).__init__()
+        self.camera_view = qtw.QWidget(parent_widget)
+        self.camera_view.setGeometry(QRect(30, 20, 871, 361))
+        self.camera_view.setObjectName("CameraView")
+        self.camera_view.setStyleSheet("background-color: white;")
 
-        self.CameraView = qtw.QWidget(ParentWidget)
-        self.CameraView.setGeometry(QRect(30, 20, 871, 361))
-        self.CameraView.setObjectName("CameraView")
-        self.CameraView.setStyleSheet('background-color: white;')
+        self.settings = settings
 
         self.warning_button = qtw.QPushButton("Warning Button")
         self.warning_button.clicked.connect(lambda: self.display_warning())
         self.video_label = qtw.QLabel()
         
-        self.warning_widget = qtw.QWidget(self.CameraView)
+        self.warning_widget = qtw.QWidget(self.camera_view)
         self.warning_widget.setGeometry(QRect(30, 20, 871, 361))
-        self.warning_symbol = QPixmap("images/warning-symbol.png").scaled(int(self.warning_widget.height()/2), int(self.warning_widget.width()/2), Qt.KeepAspectRatio, Qt.FastTransformation)
+        self.warning_symbol = QPixmap("images/warning.png").scaled(int(self.warning_widget.height()/2), int(self.warning_widget.width()/2), Qt.KeepAspectRatio, Qt.FastTransformation)
         self.warning_label = qtw.QLabel()
         self.warning_label.setPixmap(self.warning_symbol)
         
@@ -62,9 +64,9 @@ class CameraView(qtw.QWidget):
         
         layout = qtw.QVBoxLayout()
         layout.addWidget(self.warning_button)
-        layout.addWidget(self.warning_widget)
+        layout.addWidget(self.warning_widget, alignment=Qt.AlignRight | Qt.AlignBottom)
         layout.addWidget(self.video_label)
-        self.CameraView.setLayout(layout)
+        self.camera_view.setLayout(layout)
 
     @pyqtSlot(np.ndarray)
     def update_frame(self, frame):
@@ -74,32 +76,73 @@ class CameraView(qtw.QWidget):
 
 
     def display_warning(self):
-        #self.show_warning = not self.show_warning
-
-        self.media_player.play()
-        self.warning_label.setHidden(False)
+        self.show_warning = not self.show_warning
+        logging.warning("Door detect, width 72 in / 189 cm")
+        logging.critical("Door detect, width 20 in / 51 cm - WARNING!")
+        if self.settings.hasSound():
+            logging.info("sound played")
+            self.media_player.play()
+        self.warning_label.setHidden(self.show_warning)
+        
 
 class CollisionIndicatorView(qtw.QWidget):
-    def __init__(self, ParentWidget):
-        self.CollisionIndicatorView = qtw.QWidget(ParentWidget)
-        self.CollisionIndicatorView.setGeometry(QRect(29, 399, 211, 231))
-        self.CollisionIndicatorView.setObjectName("CollisionIndicatorView")
-        self.CollisionIndicatorView.setStyleSheet('background-color: white;')
+    def __init__(self, parent_widget):
+        self.collision_indicator_view = qtw.QWidget(parent_widget)
+        self.collision_indicator_view.setGeometry(QRect(29, 399, 211, 231))
+        self.collision_indicator_view.setObjectName("CollisionIndicatorView")
+        self.collision_indicator_view.setStyleSheet("background-color: white;")
 
 class LogView(qtw.QWidget):
-    def __init__(self, ParentWidget):
-        self.LogView = qtw.QWidget(ParentWidget)
-        self.LogView.setGeometry(QRect(259, 399, 411, 231))
-        self.LogView.setObjectName("LogView")
-        self.LogView.setStyleSheet('background-color: white;')
+    def __init__(self, parent_widget):
+        self.log_view = qtw.QWidget(parent_widget)
+        self.log_view.setGeometry(QRect(259, 399, 411, 231))
+        self.log_view.setObjectName("LogView")
+        self.log_view.setStyleSheet("background-color: white;")
+
+        self.log_controller = LogController(self.log_view)
+        logging.getLogger().addHandler(self.log_controller)
 
 class SettingsView(qtw.QWidget):
-    def __init__(self, ParentWidget):
-        self.SettingsView = qtw.QWidget(ParentWidget)
-        self.SettingsView.setGeometry(QRect(690, 400, 211, 231))
-        self.SettingsView.setObjectName("SettingsView")
-        self.SettingsView.setStyleSheet('background-color: white;')
+    audio_warning = True
 
+    def __init__(self, parent_widget):
+        super(SettingsView, self).__init__()
+        self.settings_view = qtw.QWidget(parent_widget)
+        self.settings_view.setGeometry(QRect(690, 400, 211, 231))
+        self.settings_view.setObjectName("SettingsView")
+        self.settings_view.setStyleSheet("background-color: white;")
+
+        self.mute_button = qtw.QPushButton(" Audio Warning")
+        self.mute_button.clicked.connect(lambda: self.toggleSound())
+        self.mute_button.setFont(QFont('Arial', 14))
+        self.mute_button.setIcon(QIcon("images/unmute.png"))
+
+        layout = qtw.QVBoxLayout()
+        layout.addWidget(self.mute_button)
+        self.settings_view.setLayout(layout)
+
+    def toggleSound(self):
+        self.audio_warning = not self.audio_warning
+        if self.audio_warning:
+            self.mute_button.setIcon(QIcon("images/unmute.png"))
+        else:
+            self.mute_button.setIcon(QIcon("images/mute.png"))
+
+    def hasSound(self):
+        return self.audio_warning
+
+class LogController(logging.Handler):
+    def __init__(self, parent_widget):
+        super(LogController, self).__init__()
+
+        self.log_text = qtw.QPlainTextEdit(parent_widget)
+        self.log_text.resize(411, 231)
+        self.log_text.setFont(QFont('Arial', 12))
+        self.log_text.setReadOnly(True)
+
+    def emit(self, msg):
+        msg = self.format(msg)
+        self.log_text.appendPlainText(msg)
 
 MEASURED_AVERAGE = 255 / 771.665  # (max_dist-min_dist)/2+min_dist then converted to 0->255 range
 
@@ -230,10 +273,10 @@ if __name__ == "__main__":
     #frame_updater_thread = camera.FrameUpdaterThread(camera_input)
     #frame_updater_thread.start()
 
-    MainWindow = qtw.QWidget()
+    main_window = qtw.QWidget()
     ui = App()
-    ui.setupUI(MainWindow)
-    MainWindow.show()
+    ui.setupUI(main_window)
+    main_window.show()
 
     pipeline = dai.Pipeline()
 
@@ -278,11 +321,11 @@ if __name__ == "__main__":
                 danger, result = analyze_frame(disparity, referenceFrame)
 
                 # cv2.imshow(WINDOW, result)
-                ui.CameraView.update_frame(result)
+                ui.camera_view.update_frame(result)
                 if danger > 5:
-                    ui.CameraView.display_warning()
+                    ui.camera_view.display_warning()
                 else:
-                    ui.CameraView.warning_label.setHidden(True)
+                    ui.camera_view.warning_label.setHidden(True)
 
                 setSlider("Danger", WINDOW, danger)
 
@@ -296,6 +339,6 @@ if __name__ == "__main__":
 
         sys.exit(app.exec_())
     except RuntimeError:
-        ui.CameraView.video_label.setText("Please plug in the depth camera and reload the program")
+        ui.camera_view.video_label.setText("Please plug in the depth camera and reload the program")
         sys.exit(app.exec_())
 
